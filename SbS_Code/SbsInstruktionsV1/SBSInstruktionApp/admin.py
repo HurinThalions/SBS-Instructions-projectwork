8fc0577ef670f79bb6a6bb46bfaca39c971a7dcd
HurinThalions <tsvetkov.svet@gmail.com>
2023-03-08 15:20:02 +0100

    admin eingerichtet

--- SbS_Code/SbsInstruktionsV1/SBSInstruktionApp/admin.py
+++ SbS_Code/SbsInstruktionsV1/SBSInstruktionApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Anleitung, Anleitungsschritt, Komponenten
+
 # Register your models here.
+
+admin.site.register(Anleitung)
+admin.site.register(Anleitungsschritt)
+admin.site.register(Komponenten)
